e6cc13a40cb2d0e49ca1377bd3e4726da21f6c81
d-das <jdawsonweehunt@gmail.com>
2019-09-03 10:58:49 -0500

    added calculator

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -21,19 +21,46 @@ export class CalculatorComponent implements OnInit {
   public rim:number = 0;
   private sWidth:number = 0;
   private sRim:number = 0;
-  public fluidOz: number = 1;
-  public handPumps: number = 3;
-  public applicationOz: number = 2;
+  public fluidOz: number = 0;
+  public handPumps: number = 0;
+  public applicationOz: number = 0;
+  public ticker: number = 0;
 
   constructor() { }
 
   ngOnInit() {}
 
-  onChange($event){
-    console.log($event);
+  calculate(){
+  if(this.sysMeasure == "metric"){
+    this.sWidth=this.width*this.factorD;
+    this.sRim=this.rim*this.factorD;
   }
+  else{
+    this.sWidth = this.width;
+    this.sRim = this.rim
+  }
+  let x:number = (this.sWidth*this.sRim)*this.factorA;
+  if(this.roadType == "offRoad"){
+    console.log("x:", x);
+    this.fluidOz = x*this.factorB;
+  }
+  else{
+    this.fluidOz =x;
+  }
+  this.applicationOz = Math.ceil(this.fluidOz/4)*4;
+  this.handPumps = this.applicationOz/this.factorC;
+
+  //round out the floats
+  this.fluidOz = parseFloat(this.fluidOz.toFixed(1));
+  this.handPumps = parseFloat(this.handPumps.toFixed(1));
+  console.log("fluidOz:", this.fluidOz);
+  console.log("handPumps:", this.handPumps);
+
+}
+
   ngDoCheck(){
-    console.log("sysMeasure:", this.sysMeasure);
-    console.log("roadType:", this.roadType);
+    this.ticker += 1;
+    this.calculate();
+    console.log("ticker:", this.ticker);
   }
 }
