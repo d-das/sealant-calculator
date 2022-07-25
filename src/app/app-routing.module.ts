82ad4df2f182f5f8066232a6d159d6d4da3ad7cc
dawson <dawson@dawsons-MacBook.local>
2022-07-24 22:30:38 -0500

    update node packages from 2019 to fix broken changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  { path: 'contact', loadChildren: './contact/contact.module#ContactPageModule' },
-  { path: 'instructions', loadChildren: './instructions/instructions.module#InstructionsPageModule' },
+  { path: 'home', 
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  { path: 'contact', 
+    loadChildren: () => import('./contact/contact.module').then( x => x.ContactPageModule) 
+  },
+  { path: 'instructions', 
+    loadChildren: () => import('./instructions/instructions.module').then( x => x.InstructionsPageModule)
+  },
 ];
 
 @NgModule({
