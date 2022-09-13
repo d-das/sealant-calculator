253ce86969fc76e009a7223dce03983fa328d248
ddas <jdawsonweehunt@gmail.com>
2022-09-13 18:26:57 -0500

    Changed routing schema, added warning button, ready for development. Added Cordova, but there are depreciated packages.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';;
 import { ComponentsModule } from '../components/components.module';
 
 import { HomePage } from './home.page';
