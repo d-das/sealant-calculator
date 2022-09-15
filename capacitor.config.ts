a52ee535fdc4f6b6b3f851aa05773d39cb42ac80
ddas <jdawsonweehunt@gmail.com>
2022-09-15 18:42:19 -0500

    fixed tslint, cordova, passed simulation tests

---
+++ capacitor.config.ts
@@ -0,0 +1,23 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sealant-calculator',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      'android-minSdkVersion': '19',
+      'android-targetSdkVersion': '28',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
+  }
+};
+
+export default config;
